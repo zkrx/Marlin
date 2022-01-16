0bb33198fc7686ea7256a52a462fbd38d675f645
zkrx <zkrx>
2022-01-16 12:48:31 +0100

    config: enable requesting long filenames with M33 command
    Needed for listing complete filenames in octoprint (with plugin).

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1449,7 +1449,7 @@
   //#define UTF_FILENAME_SUPPORT
 
   // This allows hosts to request long names for files and folders with M33
-  //#define LONG_FILENAME_HOST_SUPPORT
+  #define LONG_FILENAME_HOST_SUPPORT
 
   // Enable this option to scroll long filenames in the SD card menu
   #define SCROLL_LONG_FILENAMES
