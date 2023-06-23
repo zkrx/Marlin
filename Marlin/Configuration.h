a468958fed16ea9e5b0cf2852922b148d47b38ca
zkrx <zkrx>
2023-06-23 22:36:43 +0200

    conf: adjust endstops position

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1699,8 +1699,8 @@
 #define Y_BED_SIZE 220
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -17
-#define Y_MIN_POS -70
+#define X_MIN_POS -3
+#define Y_MIN_POS -25
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
