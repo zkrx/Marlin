230bdc6154af5aa6d5d7ce5d1f9ee86dfeeb61a8
zkrx <zkrx>
2023-08-01 10:41:48 +0200

    config: adjust bed PID params
    The bed is much slower now that it's sandwiched between to 0.5mm sheet of glass
    with the probe taped underneath the bottom glass. But the temperature is much
    more realistic than before.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -766,9 +766,10 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 47.31
-  #define DEFAULT_bedKi 3.28
-  #define DEFAULT_bedKd 454.78
+  #define DEFAULT_bedKp 23.53
+  #define DEFAULT_bedKi 0.56
+  #define DEFAULT_bedKd 654.18
+  #define MAX_CYCLE_TIME_PID_AUTOTUNE 60 // zkrx: prevent PID autotune timeout on my slow bed (default: 20min)
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #else
